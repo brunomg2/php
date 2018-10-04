62c5c1219e3acc57de535a4266b485b2f4d170e1
Bruno Gabriel <brunomg2016@hotmail.com>
2018-10-04 11:33:56 -0300

    dados sobre models

--- mvc/estrutura_mvc/controllers/homeController.class.php
+++ mvc/estrutura_mvc/controllers/homeController.class.php
@@ -1,10 +1,13 @@
 <?php
 class homeController extends Controller{
     public function index() {
+        $anuncios = new Anuncios;
+        $usuarios = new Usuarios;
+        
         $data = [
-            'quantidade' => 5,
-            'nome' => 'Bruno',
-            'idade' => 22
+            'quantidade' => $anuncios->getQuantidade(),
+            'nome' => $usuarios->getName(),
+            'idade' => $usuarios->getIdade()
         ];
         $this->loadTemplate('home',$data);
     }
---
+++ mvc/estrutura_mvc/models/Usuarios.class.php
@@ -0,0 +1,10 @@
+<?php 
+class Usuarios extends Model {
+    public function getName() {
+        return 'Bruno';
+    }
+
+    public function getIdade() {
+        return 22;
+    }
+}
